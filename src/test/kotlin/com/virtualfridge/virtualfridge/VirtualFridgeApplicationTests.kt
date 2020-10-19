e5a857b2be58f640b2fb17313991ce42d08e575c
KurekWioletta <kurek.wioletta97@gmail.com>
2020-10-19 17:57:11 +0200

    init commit

---
+++ src/test/kotlin/com/virtualfridge/virtualfridge/VirtualFridgeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.virtualfridge.virtualfridge
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VirtualFridgeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
